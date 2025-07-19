49722d1b9d399b09bb366ffc148bd8c60e89edae
Abdelhameed <abdelhameed931@gmail.com>
2025-07-19 12:29:23 +0300

    add advanced task

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,12 @@
+import time
+Monthly_income = input("Enter your monthly income : ")
+Monthly_expenses = input("Enter your monthly expenses :")
+
+monthly_savings = float(Monthly_income) - float(Monthly_expenses)
+
+print("Your monthly savings are: ", monthly_savings)
+
+time.sleep(2)
+
+print(
+    f"Projected savings after one year, with interest {.05} is: {monthly_savings * 12}")
