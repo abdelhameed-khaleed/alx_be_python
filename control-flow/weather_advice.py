5b3ec3dcf3cea3754d820afbe37a3a849315d107
Abdelhameed <abdelhameed931@gmail.com>
2025-07-19 22:26:20 +0300

    changing input variable

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,6 +1,4 @@
-weather = input(
-    "What's the weather like today? (sunny/rainy/cold): ")
-
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif weather == "rainy":
