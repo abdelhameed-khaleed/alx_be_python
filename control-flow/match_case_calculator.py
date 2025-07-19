818595e5fef6f37a47da3e021fb102a09a48801d
Abdelhameed <abdelhameed931@gmail.com>
2025-07-19 22:06:21 +0300

    adding task 0, 1,2,3 -week 3

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,25 @@
+num1 = 0
+num2 = 0
+
+
+num1 = input("Enter the first number")
+num2 = input("Enter the second number")
+operation = input("Choose the operation (+, -, *, /):")
+result = 0
+
+match (operation):
+    case "-":
+        result = float(num1) - float(num2)
+    case "*":
+        result = float(num1) * float(num2)
+    case "/":
+        if float(num2) != 0:
+            result = float(num1) / float(num2)
+        else:
+            result = "Error: Division by zero is not allowed."
+    case "+":
+        result = float(num1) + float(num2)
+    case _:
+        print("Invalid operation selected.")
+
+print(f"The result is: {result}")
---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,7 @@
+length = input("Enter the size of the pattern:")
+
+
+for i in range(1, int(length) + 1):
+    for i in range(1, int(length) + 1):
+        print("*", end="")
+    print("\n")
