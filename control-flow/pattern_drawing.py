4be7b99d27024877e43497a2dda53c118f53c7c8
Abdelhameed <abdelhameed931@gmail.com>
2025-07-19 22:37:46 +0300

    fixing taks 3

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,7 +1,15 @@
-length = input("Enter the size of the pattern:")
+size = int(input("Enter the size of the pattern: "))
 
+row = 0
 
-for i in range(1, int(length) + 1):
-    for i in range(1, int(length) + 1):
+while row < size:
+    for i in range(1, int(size) + 1):
         print("*", end="")
+    row += 1
     print("\n")
+
+
+# for i in range(1, int(size) + 1):
+#     for i in range(1, int(size) + 1):
+#         print("*", end="")
+#     print("\n")
