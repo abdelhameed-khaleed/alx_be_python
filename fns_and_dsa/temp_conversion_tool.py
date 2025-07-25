e68e02f588f782e8f796101084f5ee071e930b1c
Abdelhameed <abdelhameed931@gmail.com>
2025-07-25 18:43:29 +0300

    fix task 3

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,5 +1,5 @@
-global FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
-global CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 
 def convert_to_celsius(fahrenheit):
@@ -10,17 +10,24 @@ def convert_to_fahrenheit(celsius):
     return float(celsius) * CELSIUS_TO_FAHRENHEIT_FACTOR
 
 
-temperature = input("Enter the temperature to convert: ")
+while True:
+    temperature = input("Enter the temperature to convert: ")
+    try:
+        temperature = float(temperature)
+        break
+    except ValueError:
+        print("Please enter a valid number for temperature.")
 
-type = input(
-    "Is this temperature in Celsius or Fahrenheit? (C/F): ").lower().strip()
+while True:
+    type = input(
+        "Is this temperature in Celsius or Fahrenheit? (C/F): ").lower().strip()
+    if type in ('c', 'f'):
+        break
+    else:
+        print("Invalid input. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
 
 if type == 'c':
     print(f"{convert_to_fahrenheit(temperature)} F")
 elif type == 'f':
     print(f"{convert_to_celsius(temperature)} C")
-else:
-    print("Invalid input. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
-
-
-# what is input validations ?
