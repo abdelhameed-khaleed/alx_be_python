e9bd70804320bc54a5fbb71e20e658917caeeaba
Abdelhameed <abdelhameed931@gmail.com>
2025-07-25 18:34:35 +0300

    add task 3 week 5

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -3,3 +3,24 @@ CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 
 
 def convert_to_celsius(fahrenheit):
+    return float(fahrenheit) * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+
+def convert_to_fahrenheit(celsius):
+    return float(celsius) * CELSIUS_TO_FAHRENHEIT_FACTOR
+
+
+temperature = input("Enter the temperature to convert: ")
+
+type = input(
+    "Is this temperature in Celsius or Fahrenheit? (C/F): ").lower().strip()
+
+if type == 'c':
+    print(f"{convert_to_fahrenheit(temperature)} F")
+elif type == 'f':
+    print(f"{convert_to_celsius(temperature)} C")
+else:
+    print("Invalid input. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
+
+# what is input validations ?
