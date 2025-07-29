ebb91d9a0874c4730f906d1509b3b1d4aa5d1a89
Abdelhameed <abdelhameed931@gmail.com>
2025-07-29 20:42:49 +0300

    add print line to the divisino result

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -2,9 +2,8 @@ def safe_divide(numerator, denominator):
     try:
         num = float(numerator)
         denom = float(denominator)
-        if denom == 0:
-            raise ZeroDivisionError("Error: Cannot divide by zero.")
-    except ValueError as e:
+        return f"The result of the division is {num / denom}"
+    except ValueError:
         print(" Error: Please enter numeric values only.")
-    else:
-        return num / denom
+    except ZeroDivisionError:
+        print(" Error: Please enter numeric values only.")
