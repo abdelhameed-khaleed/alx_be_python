3781670930fda0f54bb71a23e0a22c06f836641f
Abdelhameed <abdelhameed931@gmail.com>
2025-07-29 22:09:26 +0300

    I forget something in withdraw func and added it now

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -8,6 +8,8 @@ class BankAccount:
     def withdraw(self, amount):
         if amount > self.account_balance:
             return False
+        self.account_balance -= amount
+        return True
 
     def display_balance(self):
         print(f"Current Balance: {self.account_balance}")
