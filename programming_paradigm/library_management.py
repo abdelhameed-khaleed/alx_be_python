9e024bee2a9634e4ea045efaf8e84d207c407344
Abdelhameed <abdelhameed931@gmail.com>
2025-07-29 22:06:10 +0300

    make copilot fix syntax error

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -14,7 +14,7 @@ class Book:
         return False
 
     def return_book(self):
-       if self._is_checked_out:
+        if self._is_checked_out:  # Indentation fixed here
             self._is_checked_out = False
             return True
         return False
@@ -22,24 +22,25 @@ class Book:
     def is_available(self):
         return not self._is_checked_out
 
+
 class Library:
-    def __init__(self, __books):
-        self._books = []
+    def __init__(self):  # Remove __books parameter, not needed
+        self._books = []  # Use _books consistently
 
     def add_book(self, book):
         self._books.append(book)
 
     def check_out_book(self, title):
-        for b in self.__books:
-            if b.get_title() == "title" and b.is_available():
+        for b in self._books:  # Use _books, not __books
+            if b.get_title() == title and b.is_available():  # Remove quotes around title
                 b.check_out()
 
     def return_book(self, title):
-        for b in self.__books:
-            if b.get_title() == "title" and not b.is_available():
+        for b in self._books:  # Use _books, not __books
+            if b.get_title() == title and not b.is_available():  # Remove quotes around title
                 b.return_book()
 
-    def list_avaialble_books(self):
-        for b in self.__books:
+    def list_available_books(self):  # Typo fixed in method name
+        for b in self._books:  # Use _books, not __books
             if b.is_available():
-                print(b.get_title()) 
+                print(b.get_title())
